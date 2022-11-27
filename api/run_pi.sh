d7ec0057e7abed71fe025b838fa83661c0c73aed
Christopher Lohse <christopher.lohse@ibm.com>
2022-11-27 16:41:20 +0000

    it fucking works on the pi 🎉🍺

--- api/run_pi.sh
+++ api/run_pi.sh
@@ -15,18 +15,18 @@ sleep 3
 
 # start 5 Satellites on pi 33 
 # first satellite
-ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 0 --port 33007"
+ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 0 --port 33007 --controller_address 10.35.70.40 --controller_port 33004 &" &
 # second satellite
-ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 1 --port 33008"
+ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 1 --port 33008 --controller_address 10.35.70.40 --controller_port 33004 &" &
 # third satellite
-ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 2 --port 33009"
+ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 2 --port 33009 --controller_address 10.35.70.40 --controller_port 33004 &" &
 
 # fourth satellite
-ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 3 --port 33010"
+ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 3 --port 33010 --controller_address 10.35.70.40 --controller_port 33004 &" &
 
 # fifth satellite
-ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 4 --port 33011"
+ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/satellite.py --host 10.35.70.33 --pub_id 4 --port 33011 --controller_address 10.35.70.40 --controller_port 33004 &" &
 
 # start subscriber on pi 41
 
-ssh lohsec@rasp-040.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/subscriber.py --host --host 10.35.70.40 --sub_id "cian" --port 33012"
+ssh lohsec@rasp-040.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" "export http_proxy= && cd project3/CS7NS1-Group16-content_router/api/  && python3 src/subscriber.py --host  10.35.70.40 --sub_id 'cian' --port 33012 --controller_addr 10.35.70.40:33004 &" &
