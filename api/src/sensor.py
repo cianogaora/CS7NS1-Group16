441b5f6ae5850fb8af84f625b453af18d3a169e0
OcherubO <54826937+OcherubO@users.noreply.github.com>
2022-11-27 13:24:42 +0000

    add signal strength

--- api/src/sensor.py
+++ api/src/sensor.py
@@ -69,7 +69,7 @@ class Altitude():
         self.sensor_name = 'Altitude'
         # super().__init__(sensor_name)
         self.altitude = (random.randint(300000, 500000))
-        self.final_alt = {}
+        # self.final_alt = {}
 
     def change_altitude(self):
         number = random.randint(-100, 100)
@@ -80,7 +80,7 @@ class Altitude():
                 self.altitude += 100
             elif self.altitude + number > 500000:
                 self.altitude -= 100
-            self.final_alt.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"altitude":self.altitude}})
+            # self.final_alt.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"altitude":self.altitude}})
             time.sleep(5)
 
     def generate_altitude(self):
@@ -92,7 +92,7 @@ class Altitude():
     def result_altitude(self):
         self.alt_ = {}
         self.alt_.update({time.strftime("%Y-%m-%d %H:%M:%S"): {"altitude": self.altitude}})
-        print(self.alt)
+        # print(self.alt)
         return self.alt
 
 class Angle():
@@ -102,7 +102,7 @@ class Angle():
         self.sensor_name = 'Angle'
         self.height_angle = (random.uniform(60, 90))
         self.height_angle = float(format(self.height_angle, '.2f'))
-        self.final_ang = {}
+        # self.final_ang = {}
 
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
@@ -118,7 +118,7 @@ class Angle():
                 self.height_angle += 10
             elif self.height_angle > 90:
                 self.height_angle -= 10
-            self.final_ang.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"height angle":self.height_angle}})
+            # self.final_ang.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"height angle":self.height_angle}})
             time.sleep(5)
 
     def generate_angle(self):
@@ -130,7 +130,7 @@ class Angle():
         time.sleep(1)
         self.ang = {}
         self.ang.update({time.strftime("%Y-%m-%d %H:%M:%S"): {"height angle": self.height_angle}})
-        print(self.ang)
+        # print(self.ang)
         return self.ang
 
 class Speed():
@@ -140,7 +140,7 @@ class Speed():
         self.sensor_name = 'Speed'
         self.speed = (random.uniform(6, 8))
         self.speed = float(format(self.speed, '.3f'))
-        self.final_spd = {}
+        # self.final_spd = {}
 
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
@@ -156,7 +156,7 @@ class Speed():
                 self.speed += 0.5
             elif self.speed > 8:
                 self.speed -= 0.5
-            self.final_spd.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"speed":self.speed}})
+            # self.final_spd.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"speed":self.speed}})
             time.sleep(5)
 
     def generate_speed(self):
@@ -167,7 +167,7 @@ class Speed():
     def result_speed(self):
         self.spd = {}
         self.spd.update({time.strftime("%Y-%m-%d %H:%M:%S"): {"speed": self.speed}})
-        print(self.spd)
+        # print(self.spd)
         return self.spd
 
 class Eccentricity:
@@ -178,7 +178,7 @@ class Eccentricity:
         self.sensor_name = 'Eccentricity'
         self.eccentricity = (random.uniform(0,1))
         self.eccentricity = float(format(self.eccentricity,'.2f'))
-        self.final_ecc = {}
+        # self.final_ecc = {}
 
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
@@ -194,7 +194,7 @@ class Eccentricity:
                 self.eccentricity -= 0.2
             elif self.eccentricity + eccentricity_change < 0:
                 self.eccentricity += 0.2
-            self.final_ecc.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"Orbital eccentricity":self.eccentricity}})
+            # self.final_ecc.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"Orbital eccentricity":self.eccentricity}})
             time.sleep(5)
 
     def generate_eccentricity(self):
@@ -204,7 +204,7 @@ class Eccentricity:
     def result_eccentricity(self):
         self.ecce = {}
         self.ecce.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"Orbital eccentricity":self.eccentricity}})
-        print(self.ecce)
+        # print(self.ecce)
         return self.ecce
 
 class Temperature:
@@ -215,7 +215,7 @@ class Temperature:
         self.sensor_name = 'temperature'
         self.temperature = (random.uniform(-100,100))
         self.temperature = float(format(self.temperature,'.2f'))
-        self.final_temp = {}
+        # self.final_temp = {}
 
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
@@ -231,7 +231,7 @@ class Temperature:
                 self.temperature += 5
             elif self.temperature + temperature_change > 100:
                 self.temperature += 5
-            self.final_temp.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"temperature":self.temperature}})
+            # self.final_temp.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"temperature":self.temperature}})
             time.sleep(5)
 
     def generate_temperature(self):
@@ -243,10 +243,42 @@ class Temperature:
         self.temp.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"temperature":self.temperature}})
         return self.temp
 
-class Light:
-
+class Signal:
     def __init__(self):
-        self.sensor_name = 'Light'
+        self.decimal = None
+        self.sig = None
+        self.sensor_name = 'Singal'
+        self.signal_strength = (random.uniform(-114.58,-102.27))
+        self.signal_strength = float(format(self.signal_strength,'.2f'))
+
+    def generate_decimal(self, min, max, numbers):
+        self.decimal = (random.uniform(min, max))
+        self.decimal = float(format(self.decimal, '.%df' % (numbers)))
+        return self.decimal
+
+    def change_singal(self):
+        signal_change = self.generate_decimal(-3,3,2)
+        while True:
+            if -114.58 <= self.signal_strength + signal_change <= -102.27:
+                self.signal_strength = self.signal_strength + signal_change
+            elif self.signal_strength + signal_change < -114.58:
+                self.signal_strength += 3
+            elif self.signal_strength + signal_change > -102.27:
+                self.signal_strength -= 3
+            time.sleep(5)
+
+    def generate_signal(self):
+        t1 = threading.Thread(target=self.change_singal)
+        t1.start()
+
+    def result_signal(self):
+        self.sig = {}
+        self.sig.update({time.strftime("%Y-%m-%d %H:%M:%S"): {"signal_strength": self.signal_strength}})
+        print(self.sig)
+        return self.sig
+
+
+
 
 
 
@@ -255,9 +287,11 @@ if __name__ == '__main__':
     sensor2 = Angle()
     sensor3 = Speed()
     sensor4 = Altitude()
+    sensor6 = Signal()
     sensor2.generate_angle()
     sensor5 = Eccentricity()
     sensor5.generate_eccentricity()
+    sensor6.generate_signal()
     while True:
         # sensor1.generate_location()
         # sensor1.result_location()
@@ -267,5 +301,5 @@ if __name__ == '__main__':
         # sensor3.result_speed()
         # sensor4.generate_altitude()
         # sensor4.result_altitude()
-        sensor5.result_eccentricity()
+        sensor6.result_signal()
         time.sleep(5)
