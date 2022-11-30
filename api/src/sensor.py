343aef045b739d6287d6f0e840f336fb09ddd82a
OcherubO <54826937+OcherubO@users.noreply.github.com>
2022-11-30 14:36:01 +0000

    Add some comments and solve the problem of longitude and latitude

--- api/src/sensor.py
+++ api/src/sensor.py
@@ -13,57 +13,58 @@ class Sensor:
     def __int__(self, sensor_name) -> None:
         self.sensor_name = sensor_name
 
-
+# The location contains two kinds of data (longitude and latitude)
 class Location:
+    # Latitude and longitude data generation interval
     def __init__(self):
         self.sensor_name = 'Location'
-        self.longitude = (random.uniform(-90, 90))
-        self.longitude = float(format(self.longitude, '.4f'))
-        self.latitude = (random.uniform(-180, 180))
-        self.latitude = float(format(self.latitude, ".4f"))
+        self.latitude = (random.uniform(-90, 90))
+        self.latitude = float(format(self.latitude, '.4f'))
+        self.longitude = (random.uniform(-180, 180))
+        self.longitude = float(format(self.longitude, ".4f"))
         self.final_loc = {}
-
+    # Randomly generate decimals and customize the number of decimal places
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
         self.decimal = float(format(self.decimal, '.%df' % (numbers)))
         return self.decimal
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_longitude(self):
 
-        longitude_change = self.generate_decimal(-1, 1, 4)
-        latitude_change = self.generate_decimal(0, 2, 4)
+        latitude_change = self.generate_decimal(-1, 1, 4)
+        longitude_change = self.generate_decimal(0, 2, 4)
 
         while True:
-            if 90 >= self.longitude + longitude_change >= -90:
+            if 180 >= self.longitude + longitude_change >= -180:
                 self.longitude = self.longitude + longitude_change
-            elif self.longitude + longitude_change > 90:
+            elif self.longitude + longitude_change > 180:
                 self.longitude -= 1
-            elif self.longitude + longitude_change < -90:
+            elif self.longitude + longitude_change < -180:
                 self.longitude += 1
-            if 180 >= self.latitude + latitude_change >= -180:
+            if 90 >= self.latitude + latitude_change >= -90:
                 self.latitude = self.latitude + latitude_change
-            elif self.latitude + latitude_change > 180:
+            elif self.latitude + latitude_change > 90:
                 self.latitude -= 1
-            elif self.latitude + latitude_change < -180:
+            elif self.latitude + latitude_change < -90:
                 self.latitude += 1
             self.final_loc.update({time.strftime(
                 "%Y-%m-%d %H:%M:%S"): {"longitude": self.longitude, "latitude": self.latitude}})
             time.sleep(5)
-
+    # generate location data
     def generate_location(self):
         t1 = threading.Thread(target=self.change_longitude)
         # print(self.final_loc)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_location(self):
         # print(self.longitude)
         # print(self.latitude)
         self.loc = {}
         self.loc.update({time.strftime("%Y-%m-%d %H:%M:%S")                        : {"longitude": self.longitude, "latitude": self.latitude}})
-        print(self.loc)
+        # print(self.loc)
         return self.loc
 
-
+# Altitude data
 class Altitude():
 
     def __init__(self):
@@ -72,7 +73,7 @@ class Altitude():
         # super().__init__(sensor_name)
         self.altitude = (random.randint(300000, 500000))
         # self.final_alt = {}
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_altitude(self):
         number = random.randint(-100, 100)
         while True:
@@ -84,13 +85,13 @@ class Altitude():
                 self.altitude -= 100
             # self.final_alt.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"altitude":self.altitude}})
             time.sleep(5)
-
+    # generate altitude data
     def generate_altitude(self):
         t1 = threading.Thread(target=self.change_altitude)
         # t1.setDaemon(True)
         # print(self.final_alt)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_altitude(self):
         self.alt_ = {}
         self.alt_.update({time.strftime("%Y-%m-%d %H:%M:%S")                         : {"altitude": self.altitude}})
@@ -107,11 +108,12 @@ class Angle():
         self.height_angle = float(format(self.height_angle, '.2f'))
         # self.final_ang = {}
 
+    # Randomly generate decimals and customize the number of decimal places
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
         self.decimal = float(format(self.decimal, '.%df' % (numbers)))
         return self.decimal
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_angle(self):
         angle_change = self.generate_decimal(-1, 1, 2)
         while True:
@@ -123,12 +125,12 @@ class Angle():
                 self.height_angle -= 10
             # self.final_ang.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"height angle":self.height_angle}})
             time.sleep(5)
-
+    # Generate angle data
     def generate_angle(self):
         t1 = threading.Thread(target=self.change_angle)
         # print(self.final_ang)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_anlge(self):
         time.sleep(1)
         self.ang = {}
@@ -146,11 +148,12 @@ class Speed():
         self.speed = float(format(self.speed, '.3f'))
         # self.final_spd = {}
 
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
         self.decimal = float(format(self.decimal, '.%df' % (numbers)))
         return self.decimal
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_speed(self):
         speed_change = self.generate_decimal(-1, 1, 3)
         while True:
@@ -162,12 +165,12 @@ class Speed():
                 self.speed -= 100
             # self.final_spd.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"speed":self.speed}})
             time.sleep(5)
-
+    # Generate speed data
     def generate_speed(self):
         t1 = threading.Thread(target=self.change_speed)
         # print(self.final_spd)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_speed(self):
         self.spd = {}
         self.spd.update(
@@ -186,11 +189,12 @@ class Eccentricity:
         self.eccentricity = float(format(self.eccentricity, '.2f'))
         # self.final_ecc = {}
 
+    # Randomly generate decimals and customize the number of decimal places
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
         self.decimal = float(format(self.decimal, '.%df' % (numbers)))
         return self.decimal
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_eccentricity(self):
         eccentricity_change = self.generate_decimal(-1, 1, 2)
         while True:
@@ -202,11 +206,11 @@ class Eccentricity:
                 self.eccentricity += 0.2
             # self.final_ecc.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"Orbital eccentricity":self.eccentricity}})
             time.sleep(5)
-
+    # Generate eccentricity data
     def generate_eccentricity(self):
         t1 = threading.Thread(target=self.change_eccentricity)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_eccentricity(self):
         self.ecce = {}
         self.ecce.update({time.strftime("%Y-%m-%d %H:%M:%S"): {"Orbital eccentricity": self.eccentricity}})
@@ -224,11 +228,12 @@ class Temperature:
         self.temperature = float(format(self.temperature, '.2f'))
         # self.final_temp = {}
 
+    # Randomly generate decimals and customize the number of decimal places
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
         self.decimal = float(format(self.decimal, '.%df' % (numbers)))
         return self.decimal
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_temperature(self):
         temperature_change = self.generate_decimal(-5, 5, 2)
         while True:
@@ -240,11 +245,11 @@ class Temperature:
                 self.temperature += 5
             # self.final_temp.update({time.strftime("%Y-%m-%d %H:%M:%S"):{"temperature":self.temperature}})
             time.sleep(5)
-
+    # Generate temperature data
     def generate_temperature(self):
         t1 = threading.Thread(target=self.change_temperature)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_temperature(self):
         self.temp = {}
         self.temp.update({time.strftime("%Y-%m-%d %H:%M:%S"): {"temperature": self.temperature}})
@@ -259,11 +264,12 @@ class Signal:
         self.signal_strength = (random.uniform(-114.58, -102.27))
         self.signal_strength = float(format(self.signal_strength, '.2f'))
 
+    # Randomly generate decimals and customize the number of decimal places
     def generate_decimal(self, min, max, numbers):
         self.decimal = (random.uniform(min, max))
         self.decimal = float(format(self.decimal, '.%df' % (numbers)))
         return self.decimal
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_singal(self):
         signal_change = self.generate_decimal(-3, 3, 2)
         while True:
@@ -274,11 +280,11 @@ class Signal:
             elif self.signal_strength + signal_change > -102.27:
                 self.signal_strength -= 3
             time.sleep(5)
-
+    # Generate signal strength data
     def generate_signal(self):
         t1 = threading.Thread(target=self.change_singal)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_signal(self):
         self.sig = {}
         self.sig.update({time.strftime("%Y-%m-%d %H:%M:%S")                        : {"signal_strength": self.signal_strength}})
@@ -290,7 +296,7 @@ class Microwave:
     def __init__(self):
         self.sensor_name = "Microwave"
         self.microwave_radiation = random.randint(300, 300000)
-
+    # Define the data change interval and ensure that the data does not exceed this interval after the change
     def change_microwave(self):
         microave_change = random.randint(-10, 10)
         while True:
@@ -301,11 +307,11 @@ class Microwave:
             elif self.microwave_radiation + microave_change > 300000:
                 self.microwave_radiation -= 10
             time.sleep(5)
-
+    # Generate microwave data
     def generate_microwave(self):
         t1 = threading.Thread(target=self.change_microwave)
         t1.start()
-
+    # Return the data in the form of a dictionary with timestamp as the key value
     def result_microwave(self):
         self.mic = {}
         self.mic.update({time.strftime("%Y-%m-%d %H:%M:%S")                        : {"microwave radiation": self.microwave_radiation}})
@@ -314,8 +320,8 @@ class Microwave:
 
 
 # if __name__ == '__main__':
-#     # sensor1 = Location()
-#     # sensor2 = Angle()
+#     sensor1 = Location()
+#     sensor1.generate_location()
 #     # sensor3 = Speed()
 #     # sensor4 = Altitude()
 #     # sensor6 = Signal()
@@ -325,9 +331,9 @@ class Microwave:
 #     # sensor6.generate_signal()
 #     # sensor7 = Microwave()
 #     # sensor7.generate_microwave()
-#     # while True:
+#     while True:
 #     #     # sensor1.generate_location()
-#     #     # sensor1.result_location()
+#         sensor1.result_location()
 #     #
 #     #     # sensor2.result_anlge()
 #     #     # sensor3.generate_speed()
@@ -336,4 +342,4 @@ class Microwave:
 #     #     # sensor4.result_altitude()
 #     #     # sensor6.result_signal()
 #     #     sensor7.result_microwave()
-#     #     time.sleep(5)
+#         time.sleep(5)
