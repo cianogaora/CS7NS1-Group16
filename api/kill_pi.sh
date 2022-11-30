c343e79ce3221f2f1db0052d81aa0789e967739e
Christopher Lohse <christopher.lohse@ibm.com>
2022-11-30 14:31:11 +0000

    wrote readme and changed run local and run_pi scripts

--- api/kill_pi.sh
+++ api/kill_pi.sh
@@ -1,7 +1,7 @@
 # written by Christopher
 
-ssh lohsec@rasp-033.berry.scss.tcd.ie   -o "StrictHostKeyChecking=no" pkill -9 -f contentrouter.py
-ssh lohsec@rasp-032.berry.scss.tcd.ie  -o "StrictHostKeyChecking=no" pkill -9 -f contentrouter.py
-ssh lohsec@rasp-040.berry.scss.tcd.ie  -o "StrictHostKeyChecking=no" pkill -9 -f controller.py
-ssh lohsec@rasp-040.berry.scss.tcd.ie  -o "StrictHostKeyChecking=no" pkill -9 -f subscriber.py
-ssh lohsec@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" pkill -9 -f satellite.py
+ssh username@rasp-033.berry.scss.tcd.ie   -o "StrictHostKeyChecking=no" pkill -9 -f contentrouter.py
+ssh username@rasp-032.berry.scss.tcd.ie  -o "StrictHostKeyChecking=no" pkill -9 -f contentrouter.py
+ssh username@rasp-040.berry.scss.tcd.ie  -o "StrictHostKeyChecking=no" pkill -9 -f controller.py
+ssh username@rasp-040.berry.scss.tcd.ie  -o "StrictHostKeyChecking=no" pkill -9 -f subscriber.py
+ssh username@rasp-033.berry.scss.tcd.ie -o "StrictHostKeyChecking=no" pkill -9 -f satellite.py
--- api/run.sh
+++ api/run.sh
@@ -15,7 +15,19 @@ python3 src/contentrouter.py --host localhost --port 8001  --device_id 1 --contr
 python3 src/contentrouter.py --host localhost --port 8002  --device_id 2 --controller_address localhost:8004 &
 
 sleep 3
+# first satellite
 python3 src/satellite.py --host localhost --pub_id 0 --port 8006  --controller_address localhost --controller_port 8004 &
+
+# second satellite
+python3 src/satellite.py --host localhost --pub_id 1 --port 8007  --controller_address localhost --controller_port 8004 &
+# third satellite
+python3 src/satellite.py --host localhost --pub_id 2 --port 8008  --controller_address localhost --controller_port 8004 &
+# fourth satellite
+python3 src/satellite.py --host localhost --pub_id 3 --port 8009  --controller_address localhost --controller_port 8004 &
+# fifth satellite
+python3 src/satellite.py --host localhost --pub_id 4 --port 8010  --controller_address localhost --controller_port 8004 &
+# start subscriber
+
 
 python3 src/subscriber.py --host localhost --sub_id "cian" --port 8005
 
